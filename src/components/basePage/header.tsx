8818e689006e0ab355f9eb3731a0f0d72ffb031d
jingyu <jingyu@dtstack.com>
2022-05-06 11:44:05 +0800

    feat: complete ts type and header presets

--- src/components/basePage/header.tsx
+++ src/components/basePage/header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Radio, DatePicker, Checkbox } from 'antd';
 import EasySelect from '../easySelect';
 import { HeaderProps, ItemProps } from './types';
 
 const { Item } = Form
+const { RangePicker } = DatePicker;
 export default function Header (props: HeaderProps) {
     const {
         styles, itemList, btnList,
@@ -21,12 +22,14 @@ export default function Header (props: HeaderProps) {
             >
                 {
                     itemList.length && itemList.map((item: ItemProps) => {
-                        const { type, itemOption = {}, childrenOption = {}, customRender } = item;
+                        const { type, itemOption = { label: '' }, childrenOption = {}, customRender } = item;
                         switch (type) {
                             case 'input':
                                 return (
                                     <Item {...itemOption}>
                                         <Input
+                                            key={itemOption.label as string || type}
+                                            placeholder="请输入"
                                             {...childrenOption}
                                         />
                                     </Item>)
@@ -34,6 +37,42 @@ export default function Header (props: HeaderProps) {
                                 return (
                                     <Item {...itemOption}>
                                         <EasySelect
+                                            key={itemOption.label as string || type}
+                                            placeholder="请选择"
+                                            {...childrenOption}
+                                        />
+                                    </Item>)
+                            case 'datePicker':
+                                return (
+                                    <Item {...itemOption}>
+                                        <DatePicker
+                                            key={itemOption.label as string || type}
+                                            format="YYYY-MM-DD"
+                                            placeholder="请选择"
+                                            {...childrenOption}
+                                        />
+                                    </Item>)
+                            case 'rangePicker':
+                                return (
+                                    <Item {...itemOption}>
+                                        <RangePicker
+                                            key={itemOption.label as string || type}
+                                            {...childrenOption}
+                                        />
+                                    </Item>)
+                            case 'radio':
+                                return (
+                                    <Item {...itemOption}>
+                                        <Radio.Group
+                                            key={itemOption.label as string || type}
+                                            {...childrenOption}
+                                        />
+                                    </Item>)
+                            case 'checkbox':
+                                return (
+                                    <Item {...itemOption}>
+                                        <Checkbox
+                                            key={itemOption.label as string || type}
                                             {...childrenOption}
                                         />
                                     </Item>)
--- src/components/basePage/types.tsx
+++ src/components/basePage/types.tsx
@@ -1,35 +1,37 @@
+import type { TableProps, FormItemProps, FormProps } from 'antd';
+
 export interface ItemProps {
     type: string;
-    dataSource?: object[];
-    childrenOption?: object;
-    itemOption?: object;
-    customRender?: any;
+    dataSource?: Record<string, any>[];
+    childrenOption?: Record<string, any>;
+    itemOption?: FormItemProps;
+    customRender?: FormItemProps<any>['children'];
 }
 
 export interface BtnProps {
     type?: string;
-    dataSource?: object[];
-    childrenOption?: object;
-    itemOption?: object;
+    dataSource?: Record<string, any>[];
+    childrenOption?: Record<string, any>;
+    itemOption?: FormItemProps;
 }
 
 export interface HeaderProps {
-    styles?: object;
-    initialValues?: object;
+    styles?: React.CSSProperties;
+    initialValues?: Record<string, any>;
     itemList?: ItemProps[];
     btnList?: BtnProps[];
     hideSearch?: boolean;
     hideReset?: boolean;
     onSubmit?: (value: object) => void;
-    formOption?: object;
+    formOption?: FormProps;
 }
 
 export interface ContentProps {
-    styles?: object;
-    dataSource?: object[];
-    columns: any[];
+    styles?: React.CSSProperties;
+    dataSource?: TableProps<any>['dataSource'];
+    columns: TableProps<any>['columns'];
 }
 
 export interface FooterProps {
-    styles?: object;
+    styles?: React.CSSProperties;
 }
