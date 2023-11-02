e67eb519628b20d5da2d34501b79b935939c429e
野迂迂 <yangwei1@outlook.com>
2023-11-02 20:16:02 +0800

    Updates

---
+++ markdownRender__demos.523ea455.async.js
@@ -0,0 +1,35 @@
+"use strict";(self.webpackChunkdt_react_component=self.webpackChunkdt_react_component||[]).push([[6426,2886],{34170:function(c,_,e){e.r(_);var s=e(86516),m=e.n(s),t=e(79685),a=e(73808),n=e(63342);_.default=function(){var l=(0,t.useState)(""),o=m()(l,2),r=o[0],u=o[1];return(0,t.useEffect)(function(){fetch("https://cdn.jsdelivr.net/npm/dt-react-component@3.0.8/CHANGELOG.md",{method:"get"}).then(function(d){return d.text()}).then(u)},[]),(0,n.jsx)("div",{style:{maxHeight:200,overflow:"auto",marginBottom:16},children:(0,n.jsx)(a.MarkdownRender,{value:r})})}},85198:function(c,_,e){e.r(_);var s=e(86516),m=e.n(s),t=e(79685),a=e(73808),n=e(63342),l=`
+\u4EE5\u4E0B\u662F\u4E00\u6BB5 sql \u8BED\u6CD5
+
+\`\`\`sql
+ select count(*) from a;
+-- name sqltest 
+-- type sql 
+-- create time 2022-11-09 16:13:45 
+-- desc
+
+
+-- create table employees(name string);
+insert into employees values('1111');
+
+
+select * from employees
+\`\`\`
+`;_.default=function(){var o=(0,t.useState)(""),r=m()(o,2),u=r[0],d=r[1];return(0,t.useEffect)(function(){d(l)},[]),(0,n.jsx)("div",{style:{maxHeight:400,overflow:"auto",marginBottom:16},children:(0,n.jsx)(a.MarkdownRender,{dark:!0,value:u})})}},10804:function(c,_,e){e.r(_);var s=e(86516),m=e.n(s),t=e(79685),a=e(73808),n=e(63342),l=`
+\u4EE5\u4E0B\u662F\u4E00\u6BB5 sql \u8BED\u6CD5
+
+\`\`\`sql
+ select count(*) from a;
+-- name sqltest 
+-- type sql 
+-- create time 2022-11-09 16:13:45 
+-- desc
+
+
+-- create table employees(name string);
+insert into employees values('1111');
+
+
+select * from employees
+\`\`\`
+`;_.default=function(){var o=(0,t.useState)(""),r=m()(o,2),u=r[0],d=r[1];return(0,t.useEffect)(function(){d(l)},[]),(0,n.jsx)("div",{style:{maxHeight:400,overflow:"auto",marginBottom:16},children:(0,n.jsx)(a.MarkdownRender,{value:u})})}}}]);
